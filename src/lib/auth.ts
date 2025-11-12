769af463a3fff8a0ea795db77cf36ceb96b75e3a
Odiegodev1 <odiegodev10@gmail.com>
2025-11-12 12:21:13 -0300

    Auth.js config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,15 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET!,
+  
 
   adapter: PrismaAdapter(prisma),
-  providers:[GitHub]
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  trustHost: true,
 });
